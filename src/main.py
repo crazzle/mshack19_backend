eed8b3136d50291c3e6b191ebbbdd75d109ca0fa
maces <noreply>
2019-09-27 20:26:01 +0200

    - wip endpoints

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+
 from features import features
 
 app = FastAPI()
+
 
 # TODO sqlite database with anbindung an fastapi
 
@@ -19,12 +23,40 @@ class Feature(BaseModel):
     DESCRIPTION: str
 
 
+roles = ["student", "family", "business"]
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/features")
-def read_item(response_model=Feature):
+def read_item(response_model=List[Feature]):
     # TODO use classes from above
     return features
+
+
+preselected_roles = {
+    "near_university": 3,
+    "shops": 3,
+    "nightlife": 3,
+    "public_transport": 3,
+    "avg_cost": 3
+}
+
+
+@app.get("/preselcted_features/{role}")
+def read_item(role: str):
+    if role in roles:
+        # TODO use classes from above
+        print(role)
+        return preselected_roles
+    else:
+        raise HTTPException(status_code=404, detail=f"Role not in '{', '.join(roles)}'")
+
+
+@app.post("/search")
+def read_item(query: str):
+    # TODO use classes from above
+    # selectedlist(feature: value) -> heatmap
+    return {}
