d617b4d284270f5a068780b76b83677a0cd2ceea
maces <noreply>
2019-09-27 20:41:55 +0200

    - preselected lists for roles now sorted for frontend

--- src/roles.py
+++ src/roles.py
@@ -1,25 +1,29 @@
 from constants import *
 
+# pre-sort them by importance for the frontend
+
 preselected_roles = {
     STUDENT: {
+        "nightlife": 5,
+        "avg_cost": 5,
         "near_university": 4,
         "shops": 4,
-        "nightlife": 5,
         "public_transport": 4,
-        "avg_cost": 5
+
     },
     FAMILY: {
-        "near_university": 1,
         "shops": 4,
-        "nightlife": 2,
         "public_transport": 4,
-        "avg_cost": 4
+        "avg_cost": 4,
+        "nightlife": 2,
+        "near_university": 1,
     },
     BUSINESS: {
-        "near_university": 1,
-        "shops": 2,
-        "nightlife": 1,
+        "avg_cost": 3,
         "public_transport": 3,
-        "avg_cost": 3
+        "shops": 2,
+        "near_university": 1,
+        "nightlife": 1,
+
     }
 }
