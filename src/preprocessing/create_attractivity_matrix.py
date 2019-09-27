09368366f6e6c723c235a683f01b0fac887500e1
maces <noreply>
2019-09-27 23:26:10 +0200

    - fixed

--- src/preprocessing/create_attractivity_matrix.py
+++ src/preprocessing/create_attractivity_matrix.py
@@ -19,10 +19,11 @@ feature_range = {BUS: 0.5,
 
 
 def attractiviy_matrix():
-    position_data = pd.read_csv("../../datasets/" + longlatfile)
+    position_data = pd.read_csv("../datasets/" + longlatfile)
+    position_data = position_data[:1000]
 
     for preprocfile in preprocessedfiles:
-        activity_data = pd.read_csv("../../datasets/" + preprocfile)
+        activity_data = pd.read_csv("../datasets/" + preprocfile)
         # Construct DataFrame
         data = pd.DataFrame(None, index=range(len(position_data)), columns=["Value", "Long", "Lat"])
         for position in position_data.itertuples():
@@ -37,8 +38,8 @@ def attractiviy_matrix():
                 position[1],
                 position[2],
             ]
-            with open("../../datasets/" + "final_" + preprocfile, "w") as f:
-                data.to_csv(f, index=False)
+        with open("../datasets/" + "final_" + preprocfile, "w") as f:
+            data.to_csv(f, index=False)
 
 
 if __name__ == "__main__":
