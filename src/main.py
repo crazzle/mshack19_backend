5e34792c8106bb2574ef9f2f50b759e05cda410e
maces <noreply>
2019-09-28 12:32:46 +0200

    - added cors ports

--- src/main.py
+++ src/main.py
@@ -16,9 +16,11 @@ origins = [
     "http:localhost",
     "http:localhost:8080",
     "http:localhost:8000",
+    "http:localhost:4200",
     "http:127.0.0.1",
     "http:127.0.0.1:8080",
     "http:127.0.0.1:8000",
+    "http:127.0.0.1:4200",
 ]
 
 app.add_middleware(
