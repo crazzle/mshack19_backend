85eea2e402f3d0d6cd2da4ebb5cdbe129af85a3e
Tim Sabsch <tim.sabsch@codecentric.de>
2019-09-28 13:00:15 +0200

    Normalise features

--- src/create_and_fill_db.py
+++ src/create_and_fill_db.py
@@ -7,12 +7,16 @@ import pandas as pd
 delete db and recreate it
 """
 
+def normalize(s):
+    return (s-s.min())/(s.max()-s.min())
+
 db_file = pathlib.Path.cwd().parent.joinpath('database', 'features.db')
 pathlib.Path.unlink(db_file)  # THIS DELETES THE DB
 
 db = DatabaseConnection(db_file)
 
 data = pd.read_csv("../datasets/final.csv")
+data[["public_transport","nightlife","shops","near_university","avg_cost"]] = data[["public_transport","nightlife","shops","near_university","avg_cost"]].apply(normalize, axis=0)
 
 with db:
     db.cursor.execute("""
